81f8bad45e48aa4e532eaa37ac3ce5ed4d63f46c
A <albaeftimi@gmail.com>
2025-08-22 12:03:05 +0200

    Betty

--- hsh.h
+++ hsh.h
@@ -1,13 +1,22 @@
 #ifndef HSH_H
 #define HSH_H
 
+#include <stddef.h>
+
+/* Global environment */
 extern char **environ;
 
-char **split_line(char *line);
-char *get_path(char *cmd);
-int execute_command(char **args, char *prog_name, int line_count);
-void print_error(char *prog_name, int line_count, char *cmd);
+/* Built-ins */
 void builtin_env(void);
+
+/* Command execution */
+int execute_command(char **args, char *prog_name, int line_count);
+
+/* PATH resolution */
+char *get_path(char *cmd);
 char *_getenv(const char *name);
 
-#endif
+/* Input parsing */
+char **split_line(char *line);
+
+#endif /* HSH_H */
