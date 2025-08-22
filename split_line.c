19292cda16d5427a735de654be1fefab0f9a8a42
A <albaeftimi@gmail.com>
2025-08-22 11:52:07 +0200

    Betty

--- split_line.c
+++ split_line.c
@@ -6,36 +6,37 @@
 /**
  * split_line - Tokenizes a command line into arguments
  * @line: Input command line
+ *
  * Return: Array of argument strings
  */
 char **split_line(char *line)
 {
-    int bufsize = 64, i = 0;
-    char **tokens = malloc(bufsize * sizeof(char *));
-    char *token, **tmp;
+	int bufsize = 64, i = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token, **tmp;
 
-    if (!tokens)
-        return NULL;
+	if (!tokens)
+		return (NULL);
 
-    token = strtok(line, " \t\r\n\a");
-    while (token != NULL)
-    {
-        tokens[i++] = token;
+	token = strtok(line, " \t\r\n\a");
+	while (token != NULL)
+	{
+		tokens[i++] = token;
 
-        if (i >= bufsize)
-        {
-            bufsize += 64;
-            tmp = realloc(tokens, bufsize * sizeof(char *));
-            if (!tmp)
-            {
-                free(tokens);
-                return NULL;
-            }
-            tokens = tmp;
-        }
+		if (i >= bufsize)
+		{
+			bufsize += 64;
+			tmp = realloc(tokens, bufsize * sizeof(char *));
+			if (!tmp)
+			{
+				free(tokens);
+				return (NULL);
+			}
+			tokens = tmp;
+		}
 
-        token = strtok(NULL, " \t\r\n\a");
-    }
-    tokens[i] = NULL;
-    return tokens;
+		token = strtok(NULL, " \t\r\n\a");
+	}
+	tokens[i] = NULL;
+	return (tokens);
 }
